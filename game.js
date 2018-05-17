7ee2583847d9625e924177f9950bfd5b7a440d7b
Silvia Chen <welllife1010@gmail.com>
2018-05-16 19:54:29 -0700

    Generate Score
    generating play's score

--- game.js
+++ game.js
@@ -3,3 +3,12 @@
 	遊戲JavaScript文件
 	
 */
+
+var dice = Math.floor(Math.random() * 6) + 1; // Random Number 1 - 6
+console.log(dice);
+
+var currentPlayer = 2;
+//DOM - Document Object Model
+
+// document.querySelector('#play1-current-score').textContent = dice;
+document.querySelector('#play' + currentPlayer + '-current-score').textContent = dice;
